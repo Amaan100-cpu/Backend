b558d747ad64415eced70e99c0ba4513c2398070
Amaan100-cpu <amaanahmad8616@gmail.com>
2025-09-12 17:40:43 +0530

    Update userVal.js

--- validation/userVal.js
+++ validation/userVal.js
@@ -22,5 +22,17 @@ const AdminLoginVal=z.object({
     email:z.string().trim().email({message:"Enter Valid Email"}).refine((v)=>v.includes("@gmail.com"),{message:"Enter Valid Email"})
 })
 
+const resetPassVal=z.object({
+    email:z.string().trim().email({message:"Enter Valid Email"}).refine((v)=>v.includes("@gmail.com"),{message:"Enter Valid Email"}),
+    password:z.string().trim().refine((v)=>
+        v.length>=8 &&
+        v.length<=30 &&
+        /[A-Z]/.test(v) &&
+        /[a-z]/.test(v) &&
+        /[0-9]/.test(v) &&
+        /[^A-Za-z0-9]/.test(v)
+        ,{message:"Password must be 8 character 1 letter number and special character"}
+    )
+})
 
-module.exports={registerVal,loginVal,AdminLoginVal}
+module.exports={registerVal,loginVal,AdminLoginVal,resetPassVal}
