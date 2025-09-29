6d68d1325c5fe569931beb9b38e1446599028108
Amaan100-cpu <amaanahmad8616@gmail.com>
2025-09-29 15:48:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mongoose=require("mongoose")
 const dbConnection=async()=>{
     try{
-await mongoose.connect(process.env.CONNECTION_URL)
+await mongoose.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 console.log("DB Connected")
     }
     catch(err){
         console.log("DB Connection failed")
     }
 }
-module.exports=dbConnection()
+module.exports=dbConnection
